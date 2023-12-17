2b6c80e47ee464d9389b1a94dd4378d52d429a4d
cliff <clifflee1022@gmail.com>
2023-12-17 17:59:12 +0800

    feat: add input ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { Button } from "@/components/ui/button"
+import { Textarea } from "@/components/ui/textarea"
+import { Bot, Send } from "lucide-react"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Send</Button>
+    <main className="flex min-h-screen flex-col items-center py-20 px-10 gap-6">
+      <h3 className="text-2xl font-semibold flex gap-2 items-center">
+        <Bot className="w-7 h-7" />
+        Google Gemini Pro Start
+      </h3>
+      <div className="flex flex-col items-end gap-4 w-full max-w-4xl">
+        <Textarea className="flex-1" />
+        <Button className="w-24 flex gap-2">
+          <Send className="w-4 h-4" />
+          Send
+        </Button>
+      </div>
     </main>
   )
 }
